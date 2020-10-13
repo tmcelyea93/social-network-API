6966d4e3da6f87f7deffb3123230cca5618d7c74
tmcelyea93 <tarahmcelyea@gmail.com>
2020-10-12 21:47:44 -0500

    routes and index

--- server.js
+++ server.js
@@ -21,4 +21,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-
 mongoose.set('debug', true);
 
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
